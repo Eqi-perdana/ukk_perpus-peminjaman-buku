17e7ca2e788fa3ffa21570cac29d99edda5dad54
Eqi-perdana <perdanaeqi@gmail.com>
2026-01-14 10:28:38 +0700

    develop:"database-migration-siswa"

---
+++ database/migrations/2026_01_14_030854_create_siswas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('siswas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nit')->unique();          // Nomor Induk Siswa
+            $table->string('nama');
+            $table->string('kelas');
+            $table->string('jurusan');
+            $table->enum('jenis_kelamin', ['L', 'P']);
+            $table->string('no_telp')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('siswas');
+    }
+};
